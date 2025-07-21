bea8510461d411b4d222648276d4389688a15401
Callum-James Smith <sudo-make99@protonmail.com>
2025-07-21 18:00:35 +0100

    Implemented basic player movement; walk left and right + jumping

---
+++ Assets/Scripts/Character/GroundCheck.cs
@@ -0,0 +1,23 @@
+using System;
+using UnityEngine;
+
+namespace Character
+{
+    public class GroundCheck : MonoBehaviour
+    {
+        private PlayerMovementController _playerMovementController;
+
+        private void Start()
+        {
+            _playerMovementController = GetComponentInParent<PlayerMovementController>();
+        }
+
+        private void OnCollisionEnter2D(Collision2D other)
+        {
+            if (other.gameObject.CompareTag("Ground"))
+            {
+                _playerMovementController.IsPlayerGrounded = true;
+            }
+        }
+    }
+}
--- Assets/Scripts/Character/Player Movement Controller.cs
+++ Assets/Scripts/Character/Player Movement Controller.cs
@@ -1,3 +1,4 @@
+using System;
 using UnityEngine;
 using UnityEngine.InputSystem;
 
@@ -16,26 +17,35 @@ namespace Character
 
         // PUBLIC
         public MoveActionTypes currentMoveAction;
+        public bool IsPlayerMoving { get; set; }
+        public bool IsPlayerGrounded { get; set; }
 
         #region SERIALIZED_MEMBER_VARIABLES
 
         [Header("Movement")]
         
-        [Range(0f, 1f)]
+        // [Range(0f, 1f)]
         [SerializeField] private float maximumAcceleration;
+
+        [SerializeField] private float deceleration;
+        [SerializeField] private float jumpAcceleration;
         [SerializeField] private float walkingSpeed;
         [SerializeField] private float runningSpeed;
         [SerializeField] private float rollingSpeed;
         [SerializeField] private int jumpHeight;
+        [SerializeField] private float fallMultiplier;
 
         [Header("Object References")]
         [SerializeField] private InputActionReference moveActionInput;
+
+        [SerializeField] private InputActionReference jumpActionInput;
 
         #endregion
 
         private Rigidbody2D _rigidbody;
         private Vector2 _moveDirection;
         private SpriteRenderer _spriteRenderer;
+        private float _currentSpeed;
 
         private void Start()
         {
@@ -44,34 +54,70 @@ namespace Character
         }
 
         private void Update()
-        { 
-            _moveDirection = moveActionInput.action.ReadValue<Vector2>().normalized;
+        {
+            
         }
 
         private void FixedUpdate()
         {
             UpdateMovement();
             UpdateOrientation();
+
+            if (IsPlayerGrounded != true)
+            {
+                // Apply extra gravity when player is falling
+                _rigidbody.linearVelocity +=
+                    Vector2.up * Physics2D.gravity.y * (fallMultiplier - 1) * Time.fixedDeltaTime;
+            }
+            
         }
 
         /// <summary>
-        /// Move this player towards a target position using the moveAction parameter to figure out the
-        /// correct speed to use.
+        /// Handles the player's movement systems; left, right, and jump.
+        /// Called by InputAction Callback.
         /// </summary>
-        /// <param name="moveAction">The type of movement (walking, running, rolling).</param>
-        /// <param name="targetPosition">The world position of the final destination.</param>
         private void UpdateMovement()
         {
-            _rigidbody.linearVelocity += _moveDirection * maximumAcceleration;
-            _rigidbody.linearVelocity.Normalize();
-            _rigidbody.linearVelocity *= walkingSpeed;
+            var targetSpeedX = _moveDirection.x * walkingSpeed;
+            var speedDifferenceX = targetSpeedX - _rigidbody.linearVelocityX;
+            var accelerationRateX = (Mathf.Abs(targetSpeedX) > 0.01f) ? maximumAcceleration : deceleration;
+            
+            var movementX = Mathf.Clamp(speedDifferenceX, -maximumAcceleration * Time.fixedDeltaTime, accelerationRateX * Time.fixedDeltaTime);
+            _rigidbody.linearVelocity = new Vector2(_rigidbody.linearVelocityX + movementX, _rigidbody.linearVelocityY);
+            
+
+            IsPlayerMoving = _rigidbody.linearVelocity.magnitude != 0;
+        }
+
+        /// <summary>
+        /// Called by the InputAction callback. Updates the _moveDirection variable whenever the player
+        /// activates the Jump action.
+        /// </summary>
+        /// <param name="context"></param>
+        public void UpdateMoveDirection(InputAction.CallbackContext context)
+        {
+            _moveDirection = moveActionInput.action.ReadValue<Vector2>().normalized;
+        }
+
+        public void Jump(InputAction.CallbackContext context)
+        {
+            // Make the character jump when they press the JUMP key.
+            if (IsPlayerGrounded)
+            {
+                // Make the character jump
+                IsPlayerGrounded = false;
+                _rigidbody.linearVelocityY += ((jumpHeight * jumpAcceleration) + Mathf.Abs(_rigidbody.linearVelocity.normalized.x));
+            }
         }
 
         private void UpdateOrientation()
         {
             if (_rigidbody.linearVelocity.magnitude > 0)
             {
-                _spriteRenderer.flipX = _rigidbody.linearVelocityX < 0;
+                if (_moveDirection.x < 0)
+                    _spriteRenderer.flipX = true;
+                else if (_moveDirection.x > 0)
+                    _spriteRenderer.flipX = false;
             }
         }
     }
