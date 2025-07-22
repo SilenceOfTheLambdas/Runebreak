bc6d975e3b10fe3abd26d6b779036e6a4f65c3d0
Callum-James Smith <sudo-make99@protonmail.com>
2025-07-22 13:44:18 +0100

    started implementing Character RPG Systems

---
+++ Assets/Scripts/Character/RPGSystem.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+
+namespace Character
+{
+    public class RPGSystem : MonoBehaviour
+    {
+        [Header("Core Stats")]
+        public int maximumHealth = 10;
+        public int maximumStamina = 20;
+        public int maximumResolve = 10;
+        public int currentLevel = 1;
+
+        [Tooltip("(1 unit per x) where x is the value of this variable.")] [Range(0.01f, 1f)]
+        public float healthRegenRate;
+
+        [Tooltip("(1 unit per x) where x is the value of this variable.")] [Range(0.01f, 1f)]
+        public float staminaRegenRate;
+
+        [Tooltip("(1 unit per x) where x is the value of this variable.")] [Range(0.01f, 1f)]
+        public float resolveRegenRate;
+        
+        private int _currentHealth;
+        private int _currentStamina;
+        private int _currentResolve;
+
+        private void Start()
+        {
+            _currentHealth = maximumHealth;
+            _currentStamina = maximumStamina;
+            _currentResolve = maximumResolve;
+        }
+
+        /// <summary>
+        /// Deals x amount of damage to this character. This function will also for player death.
+        /// </summary>
+        /// <param name="damage">The amount of damage to take.</param>
+        public void ReceiveDamage(int damage)
+        {
+            _currentHealth -= damage;
+        }
+    }
+}
